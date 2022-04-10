f9d2fe74b76a3b41ec70e7a936f7a8f5bc515f47
habeebahmed <habeebahmed900@gmail.com>
2022-04-11 02:59:22 +0530

    feat(input): input field

---
+++ src/TextField/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TextField(props) {
+    return (
+        <div>
+            <input type="text" value="Empty" />
+        </div>
+    );
+}
+
+export default TextField;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-
+import TextField from './TextField'
 export default class extends Component {
   render() {
     return <div>
       <h2>Welcome to React components</h2>
+      <TextField />
     </div>
   }
 }
