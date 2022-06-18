88cce5b9d8925f1f43b9ec6b874ee86b474b258d
habeebahmed <habeebahmed900@gmail.com>
2022-06-18 13:41:53 +0530

    Revert "docs(textfield): just console log"
    This reverts commit c3667d989b480dc5eacb805dcb9a42cf8e06819d.

--- src/TextField/index.js
+++ src/TextField/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function TextField(props) {
-    console.log("Text Field", props);
     return (
         <div>
             <input type="text" value="Empty" />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react'
 import TextField from './TextField'
-import ReactInputFieldTest from 'react-components-test-hm';
 export default class extends Component {
   render() {
     return <div>
       <h2>Welcome to React components</h2>
       <TextField />
-      <ReactInputFieldTest />
     </div>
   }
 }
